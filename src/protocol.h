348e2fe70c760b89d13aaf746430bc3e3ed6c8c7
BitcoinEA <bitcoinea@protonmail.com>
2018-01-20 09:03:43 +0300

    port change

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 39403 : 29406;
+    return testnet ? 39403 : 29506;
 }
 
 
